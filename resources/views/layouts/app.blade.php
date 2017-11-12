ae6c1fa9b3f755961204be107ee37d0aea59f31d
aipopablo <pablo.93jeje@gmail.com>
2017-11-12 19:39:21 -0300

    seguimos con el commiteo, todos los campos del pedidos y algo de lo que va a ser la inserción

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,6 +103,7 @@
     <!--Aca le agregamos la boludez esta del datetimepicker-->
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    @stack('scripts')
 
     <script>
         $( function() {
--- resources/views/pedidos/form.blade.php
+++ resources/views/pedidos/form.blade.php
@@ -38,7 +38,7 @@
 
 	<div class="panel panel-primary">
 		<div class="panel-body">
-			<div class = "col-lg-3 col-sm-3 col-md-3 col-xs-12">
+			<div class = "col-lg-4 col-sm-4 col-md-4 col-xs-12">
 				<div class="form-group">
 					<label>Articulo</label>
 					<select name="pid_articulo" class="form-control selectpicker" id="pid_articulo" data-live-search="true">
@@ -50,34 +50,96 @@
 					</select>	
 				</div>
 			</div>
-			<div class = "col-lg-3 col-sm-3 col-md-3 col-xs-12">	
+			<div class = "col-lg-3 col-sm-2 col-md-2 col-xs-12">	
 				<div class="form-group">
 					<label>Cantidad</label>
 					<input type="number" name="pcantidad" id="pcantidad" class="form-control" placeholder="cantidad"></input>
 				</div>
 			</div>
-			<div class = "col-lg-3 col-sm-3 col-md-3 col-xs-12">	
+			<div class = "col-lg-3 col-sm-3 col-md-2 col-xs-12">	
 				<div class="form-group">
 					<label>Prec. Unit.</label>
-					<input type="number" name="pprecio_unitario" id="pprecio_unitario" class="form-control" placeholder="cantidad"></input>
+					<input type="number" name="pprecio_unitario" id="pprecio_unitario" class="form-control" placeholder="cantidad">
+					</input>
 				</div>
 			</div>
-			<div class = "col-lg-3 col-sm-3 col-md-3 col-xs-12">	
-				<div class="form-group">
-					<label>Total Artic.</label>
-					
-				</div>
+			<div class = "col-lg-2 col-sm-2 col-md-2 col-xs-12">	
+				<button type="button" id="btn_add" class="btn btn-primary">Agregar</button>	
+			</div>
+
+			<div class ="col-lg-12 col-sm-12 col-md-12 col-xs-12">	
+				<table id="detalles" class="table table-stripped table-bordered table-condensed table-hover table-sm .table-responsive">
+					<thead>
+						<th>Opciones</th>
+						<th>Articulo</th>
+						<th>Cantidad</th>
+						<th>Precio Unit.</th>
+						<th>SubTotal</th>
+					</thead>
+					<tfoot>
+						<th>Total</th>
+						<th></th>
+						<th></th>
+						<th></th>
+						<th><h4 id="total">S/. 0.00</h4></th>				
+					</tfoot>
+					<tbody>
+						
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
 
-	<div class="form-group">
-		<label>Total</label>
-
-	</div>
-
 	<div class="form-group text-right">
 		<a href="{{url('/productos')}}">Regresar al listado de Pedidos</a>
-		<input type="submit" value="enviar" class="btn btn-success">
+		<input id="guardar" type="submit" value="Guardar" class="btn btn-success">
 	</div>
 {!!Form::close()!!}
+
+@push('scripts')
+<script>
+	$(document).ready(function(){
+		$('#btn_add').click(function () {
+			agregar(); 
+		});
+	});
+
+	var cont = 0;
+	total=0;      
+	subtotal=[];
+
+	function agregar()
+	{
+		idarticulo=$("#pid_articulo").val();
+		articulo=$("#pid_articulo option:selected").text();
+		cantidad=$("#pcantidad").val();
+		precio_unitario=$("#pprecio_unitario").val();
+
+		if(idarticulo!="" && cantidad!="" && cantidad>0 && precio_unitario!="")
+		{
+			subtotal[cont] = cantidad*precio_unitario;
+			tota=total+subtotal[cont];
+
+			var fila =' <tr class="selected" id"fila '+cont+'" > <td></td> </tr>';
+		}
+	}
+
+	function limpiar() {
+		$("#pcantidad").val("");
+		$("#pprecio_unitario").val("");				
+	}
+
+	function evaluar() {
+		if (total > 0) 
+		{
+			$("#guardar").show;
+		} 
+		else 
+		{
+			$("#guardar").hide;
+		}
+	}
+
+</script>
+@endpush
