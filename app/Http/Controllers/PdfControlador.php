fb56004fa51a7bd469ffad84f23ee02d5a970017
aipopablo <pablo.93jeje@gmail.com>
2017-11-30 11:08:52 -0300

    afasfd

--- app/Http/Controllers/PdfControlador.php
+++ app/Http/Controllers/PdfControlador.php
@@ -42,8 +42,8 @@ class PdfControlador extends Controller
     	$pdf = \App::make('dompdf.wrapper');
     	$pdf->loadHTML($view);
 
-    	if ($tipo == 1) {return $pdf->stream('reporte.pdf');}
-    	if ($tipo == 2) {return $pdf->download('reporte.pdf');}	
+    	if ($tipo == 1) {return $pdf->stream('listadopedidos.pdf');}
+    	if ($tipo == 2) {return $pdf->download('listadopedidos.pdf');}	
     }
 
     //a partir de aca, creamos las consultas para los reportes, 
